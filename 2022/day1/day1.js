a8c79685ff5755ff40487347fcf4eed9b9163643
Thegajout <gjuliens10@gmail.com>
2022-12-02 10:46:23 +0000

    format

--- 2022/day1/day1.js
+++ 2022/day1/day1.js
@@ -1,7 +1,7 @@
-const {readFileSync} = require('fs');
-const {resolve} = require('path')
+const { readFileSync } = require('fs');
+const { resolve } = require('path')
 function syncReadFile(filename) {
-    const contents = readFileSync(resolve(__dirname, filename), "utf-8");
+  const contents = readFileSync(resolve(__dirname, filename), "utf-8");
 
   const arr = contents.split(/\r?\n/);
 
@@ -15,9 +15,9 @@ const s = output
   .split(',,')
   .map(v =>
     v
-    .split(',')
-    .reduce((a,b) => +a + +b, 0)
-    )
+      .split(',')
+      .reduce((a, b) => +a + +b, 0)
+  )
 
 console.log(Math.max(...s)) //Step 1
-console.log(s.sort((a,b) => b - a).slice(0,3).reduce((a,b) => a+b, 0) ) //Step 2
+console.log(s.sort((a, b) => b - a).slice(0, 3).reduce((a, b) => a + b, 0)) //Step 2
--- 2022/day2/day2.js
+++ 2022/day2/day2.js
@@ -1,92 +1,85 @@
-const { readFileSync, } = require('fs');
-const {resolve} = require('path')
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
 function syncReadFile(filename) {
-    const contents = readFileSync(resolve(__dirname, filename), "utf-8");
+  const contents = readFileSync(resolve(__dirname, filename), "utf-8");
 
-    const arr = contents.split(/\r?\n/);
+  const arr = contents.split(/\r?\n/);
 
-    return arr;
+  return arr;
 }
-const output = syncReadFile('./day2.txt');
-console.log(output)
+const output = syncReadFile("./day2.txt");
+console.log(output);
 
 const scoresForOutcome = {
-    winning: 6,
-    loosing: 0,
-    draw: 3
-}
+  winning: 6,
+  loosing: 0,
+  draw: 3,
+};
 
 const scoresForChosenStrategy = {
-    rock: 1,
-    paper: 2,
-    scissors: 3
-}
+  rock: 1,
+  paper: 2,
+  scissors: 3,
+};
 
 const winning = {
-    rock: "scissors",
-    paper: "rock",
-    scissors: "paper"
-}
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper",
+};
 
 const mappingTemplate = {
-    A: "rock",
-    B: "paper",
-    C: "scissors",
-    X: "rock",
-    Y: "paper",
-    Z: "scissors"
-}
+  A: "rock",
+  B: "paper",
+  C: "scissors",
+  X: "rock",
+  Y: "paper",
+  Z: "scissors",
+};
 
 const step2MappingTemplate = {
-    ...mappingTemplate,
-    X: "loosing",
-    Y: "draw",
-    Z: "winning"
-}
-
+  ...mappingTemplate,
+  X: "loosing",
+  Y: "draw",
+  Z: "winning",
+};
 
 const step1 = output
-    .map(value => value
-        .split(" ")
-        .map(value => mappingTemplate[value])
-    )
-    .map(value => {
-        const firstElf = value[0]
-        const secondElf = value[1]
-        if (winning[firstElf] === secondElf) {
-            // first elf wins
-            return scoresForChosenStrategy[secondElf] + scoresForOutcome.loosing
-        } else if (winning[secondElf] === firstElf) {
-            // You win / second elf wins
-            return scoresForChosenStrategy[secondElf] + scoresForOutcome.winning
-        } else {
-            //draw
-            return scoresForChosenStrategy[secondElf] + scoresForOutcome.draw
-        }
+  .map((value) => value.split(" ").map((value) => mappingTemplate[value]))
+  .map((value) => {
+    const firstElf = value[0];
+    const secondElf = value[1];
+    if (winning[firstElf] === secondElf) {
+      // first elf wins
+      return scoresForChosenStrategy[secondElf] + scoresForOutcome.loosing;
+    } else if (winning[secondElf] === firstElf) {
+      // You win / second elf wins
+      return scoresForChosenStrategy[secondElf] + scoresForOutcome.winning;
+    } else {
+      //draw
+      return scoresForChosenStrategy[secondElf] + scoresForOutcome.draw;
     }
-    ).reduce((a, b) => a + b, 0)
+  })
+  .reduce((a, b) => a + b, 0);
 
-console.log(step1)
+console.log(step1);
 
 const step2 = output
-.map(value => value
-    .split(" ")
-    .map(value => step2MappingTemplate[value])
-    ).map(value => {
-        const firstElf = value[0]
-        const outcome = value[1]
-        if (outcome === "winning"){
-            const yourPlay = winning[winning[firstElf]]
-            return scoresForChosenStrategy[yourPlay] + scoresForOutcome[outcome]
-        } else if (outcome === "loosing") {
-            const yourPlay = winning[firstElf]
-            return scoresForChosenStrategy[yourPlay] + scoresForOutcome[outcome]
-        } else {
-            const yourPlay = firstElf
-            return scoresForChosenStrategy[yourPlay] + scoresForOutcome[outcome]
-        }
+  .map((value) => value.split(" ").map((value) => step2MappingTemplate[value]))
+  .map((value) => {
+    const firstElf = value[0];
+    const outcome = value[1];
+    if (outcome === "winning") {
+      const yourPlay = winning[winning[firstElf]];
+      return scoresForChosenStrategy[yourPlay] + scoresForOutcome[outcome];
+    } else if (outcome === "loosing") {
+      const yourPlay = winning[firstElf];
+      return scoresForChosenStrategy[yourPlay] + scoresForOutcome[outcome];
+    } else {
+      const yourPlay = firstElf;
+      return scoresForChosenStrategy[yourPlay] + scoresForOutcome[outcome];
     }
-    ).reduce((a,b) => a + b, 0)
+  })
+  .reduce((a, b) => a + b, 0);
 
-console.log(step2)
-
+console.log(step2);
