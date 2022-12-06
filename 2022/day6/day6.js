a67d886bc4d35f85c25e9e581d03bb8906601081
Thegajout <gjuliens10@gmail.com>
2022-12-06 06:17:15 +0000

    day 6

---
+++ 2022/day6/day6.js
@@ -0,0 +1,19 @@
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
+function syncReadFile(filename) {
+    const contents = readFileSync(resolve(__dirname, filename), "utf-8");
+
+    const arr = contents.split("");
+
+    return arr;
+}
+const output = syncReadFile("./day6.txt");
+console.log(output);
+
+const step1 = output
+    .findIndex((_, index) => index - 4 >= 0 ? [...new Set(output.slice(index - 4, index))].length === 4 : false)
+const step2 = output
+    .findIndex((_, index) => index - 14 >= 0 ? [...new Set(output.slice(index - 14, index))].length === 4 : false)
+
+console.log(step1)
+console.log(step2)
